89a4fc03d1dfe01dd713342485c62788c444b8f7
jingtong1 <1234qwer>
2018-08-17 19:24:01 +0800

    home list commit

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, MainComponent],
   imports: [
     BrowserModule,
     NxModule.forRoot(),
+    HttpClientModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
   providers: [],
--- apps/web/src/app/main/main.component.ts
+++ apps/web/src/app/main/main.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { trigger } from "@angular/animations";
 
 @Component({
   selector: 'guide-client-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  animations:[
+    trigger('guideState',[
+      state('inactive',style({}))
+    ])
+  ]
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
+  hotTopic:any;
+  experience:any;
+  route:any;
+  ceng_display:boolean;
+  circle_display:boolean;
+  constructor(private http:HttpClient) {
+        this.ceng_display = true;
+        this.circle_display = false;
+  }
 
   ngOnInit() {
+    this.http.get("http://localhost:3000/hotTopic")
+      .subscribe(res=>{this.hotTopic=res})
+    this.http.get("http://localhost:3000/experience")
+      .subscribe(res=>{this.experience=res})
+    this.http.get("http://localhost:3000/route")
+      .subscribe(res=>{this.route=res})
+
   }
 
 }
